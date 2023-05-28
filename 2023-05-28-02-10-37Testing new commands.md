db33da55090cfb534c8f369df53d728fa737f9f2
zjreid1 <zjreid1@gmail.com>
2023-05-28 02:57:49 -0500

    adding blog post

---
+++ 2023-05-28-02-10-37Testing new commands.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: Testing new commands 
+date: 2023-05-28-02-10-37
+categories: [Uncategorized]
+---
+
+Testing new commands
--- postGenerator.py
+++ postGenerator.py
@@ -10,12 +10,16 @@ date:   2022-09-22 02:38:20 -0700
 categories: jekyll update
 ---
 
+
 """
+
 
 # Creating a Jekyll post template in Python
 
 from datetime import datetime
 import os
+import subprocess
+import shutil
 
 #Creating the time variable to reference in the post
 t = datetime.now()
@@ -32,7 +36,33 @@ newFile.write("title: " + posttitle + " \n")
 newFile.write("date: " + timestamp + "\n")
 newFile.write("categories: [Uncategorized]\n")
 newFile.write("---\n")
-
 newFile.close()
+
+# Open notepad and title the resulting file
+print('notepad opened: ' + str(datetime.now()))
 osCommandString = "notepad.exe" + " " + timestamp + posttitle + ".md"
-os.system(osCommandString)
+subprocess.call(osCommandString)
+print('notepad closed: ' + str(datetime.now()))
+
+# Upon user closure of notepad, copy the file to "posts_"
+dataString = timestamp + posttitle + ".md"
+originPath = os.getcwd()
+targetPath = originPath + r"\_posts"
+shutil.copy(originPath + '\\' + dataString, targetPath + '\\' + dataString)
+
+
+
+# Git Commit
+subprocess.run(['git', 'add', '.'])
+print("git add completed")
+subprocess.run(['git', 'commit', '-m', 'adding blog post'])
+print("git commit with commit message completed")
+
+"""
+# Git Commit
+subprocess.Popen("git add .")
+print("git add completed")
+subprocess.Popen('git commit -m' " adding blog post")
+print("git commit with commit message completed")
+print("ready for git push origin main")
+"""
